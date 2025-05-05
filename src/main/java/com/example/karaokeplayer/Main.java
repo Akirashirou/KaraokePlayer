9adb9ac0ae895ace072c9e3ba5dc7aaffc17d1d0
Akirashirou <150439855+Akirashirou@users.noreply.github.com>
2025-05-06 00:01:02 +0800

    added a logo
    logo in the landing page and also in the title bar

--- src/main/java/com/example/karaokeplayer/Main.java
+++ src/main/java/com/example/karaokeplayer/Main.java
@@ -3,6 +3,7 @@ package com.example.karaokeplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,11 @@ public class Main extends Application {
         // Load the landing page FXML instead of media player FXML
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("landing-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        Image logo = new Image(getClass().getResourceAsStream("MicLogo.jpg"));
+        stage.getIcons().add(logo);
+
+
         stage.setTitle("Welcome");
         stage.setScene(scene);
         stage.show();
